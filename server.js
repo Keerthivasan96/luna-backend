4a64d96193fa4ae4dbf95aadae69dc4521c219f8
Keerthivasan96 <keerthivasanrs96@gmail.com>
2025-12-24 05:53:03 +0530

    upgrading backend 5

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
-
+import fetch from "node-fetch";
 dotenv.config();
 
 const app = express();
